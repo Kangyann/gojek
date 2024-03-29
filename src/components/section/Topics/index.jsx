1d8cea22c1562e16159ec8a3a3c9d734a0c78983
Kangyann <kangyann727@gmail.com>
2024-03-30 05:33:16 +0700

    Update Help Page

--- src/components/section/Topics/index.jsx
+++ src/components/section/Topics/index.jsx
@@ -8,7 +8,7 @@ const Topics = () => {
                     <h1>Browse Topics</h1>
                     <div className="topics__menu">
                         {Data.Topics.map((x, y) => (
-                            <Link className="topics__menu__item">
+                            <Link to={x.link} className="topics__menu__item">
                                 <img src={x.image} alt="" width={30} height={30} />
                                 <span>{x.title}</span>
                             </Link>
